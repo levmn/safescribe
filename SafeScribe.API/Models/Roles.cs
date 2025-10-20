0f4d82513ac4a27fff0b197b250a03f5d0dc4086
levmn <levimncontato@gmail.com>
2025-10-20 20:38:45 -0300

    feat: adding model entities

---
+++ SafeScribe.API/Models/Roles.cs
@@ -0,0 +1,9 @@
+namespace SafeScribe.API.Models
+{
+    public static class Roles
+    {
+        public const string Leitor = "Leitor";
+        public const string Editor = "Editor";
+        public const string Admin = "Admin";
+    }
+}
